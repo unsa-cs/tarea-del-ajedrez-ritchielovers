5f02fa9f9a0e4d015666a60065a671cdc1ffae11
CHOCEK <chanaricu@unsa.edu.pe>
2024-10-04 21:57:02 -0500

    Se realizo una combinacion de funciones probando principalmente la funcion up para llenar todo el tablero con las figuras de los caballos

--- ejemplo.c
+++ ejemplo.c
@@ -9,5 +9,8 @@ void display() {
   char **four_knight = join(two_knight, flipv_two_knight);
   char **fliph_four_knight = flipH(four_knight);
   char **eight_knight = join(four_knight, fliph_four_knight);
-  interpreter(eight_knight);
+  char **sixteen_knight = up(eight_knight, eight_knight);
+  char **thirtytwo_knight = up(sixteen_knight, reverse(sixteen_knight));
+  char **sixtyfour_knight = up(thirtytwo_knight, thirtytwo_knight);
+  interpreter(sixtyfour_knight);
 }
