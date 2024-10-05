8e280be513bd8c24758f52510f14e60bcda1c340
SergioCalcinaMuchica <scalcinamu@unsa.edu.pe>
2024-10-04 22:51:34 -0500

    Continuando con el ejercicio 1, se ha hecho uso de la función repeatH para crear una fila de casilleros

--- ejemplo.c
+++ ejemplo.c
@@ -3,10 +3,11 @@
 
 void display() {
   
-  char **_spwhite = whiteSquare;
-  char **_spblack = reverse(_spwhite);
-  char **two_block = join(_spwhite, _spblack);
   
+  char **_spwhite = whiteSquare; //un casillero en blanco
+  char **_spblack = reverse(_spwhite); //un casillero en negro
+  char **two_block = join(_spwhite, _spblack); //dos casilleros, uno blanco y uno negro
+  char **fila = repeatH(two_block,4); //fila de casilleros blancos y negros
 
-  interpreter(two_block);
+  interpreter(fila);
 }
