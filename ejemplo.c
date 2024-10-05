0ad7d4676fc414287b6fb4d48bf232952eddd9ff
SergioCalcinaMuchica <scalcinamu@unsa.edu.pe>
2024-10-04 23:07:16 -0500

    Experimento para resolver de manera diferente el ejercicio 1, usando solamente la funcion join pero no es tan util porque no se entiende bien a primera vista

--- ejemplo.c
+++ ejemplo.c
@@ -6,8 +6,8 @@ void display() {
   
   char **_spwhite = whiteSquare; //un casillero en blanco
   char **_spblack = reverse(_spwhite); //un casillero en negro
-  char **two_block = join(_spwhite, _spblack); //dos casilleros, uno blanco y uno negro
-  char **fila = repeatH(two_block,4); //fila de casilleros blancos y negros
+  //char **two_block = join(_spwhite, _spblack); //dos casilleros, empezando con uno negro y luego otro blanco
+  char **fila = join(join(join(_spwhite, _spblack),join(_spwhite, _spblack)),join(join(_spwhite,_spblack),join(_spwhite,_spblack)));
 
   interpreter(fila);
 }
