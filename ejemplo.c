ab9ea5c9ccc931fa03ed98307316ba0aafcff986
SergioCalcinaMuchica <scalcinamu@unsa.edu.pe>
2024-10-04 22:38:28 -0500

    Avance del ejercicio 1, combinando join y reverse

--- ejemplo.c
+++ ejemplo.c
@@ -2,20 +2,11 @@
 #include "figures.h"
 
 void display() {
-  char **_knight = knight;
-  char **_black_knight = reverse(_knight);
-  char **two_knight = join(_knight, _black_knight);
-  char **flipv_two_knight = flipV(two_knight);
-  /*
-  char **four_knight = join(two_knight, flipv_two_knight);
-  char **fliph_four_knight = flipH(four_knight);
-  char **eight_knight = join(four_knight, fliph_four_knight);
-  char **sixteen_knight = up(eight_knight, eight_knight);
-  char **thirtytwo_knight = up(sixteen_knight, reverse(sixteen_knight));
-  char **sixtyfour_knight = up(thirtytwo_knight, thirtytwo_knight);
-  char **rotatel_knight = rotateL(_knight);
-  */
-  char **flipv_rotater_two_knight = rotateR(flipv_two_knight);
-  char **repeath_flipv_rotater_two_knight = repeatH(flipv_rotater_two_knight, 8);
-  interpreter(repeath_flipv_rotater_two_knight);
+  
+  char **_spwhite = whiteSquare;
+  char **_spblack = reverse(_spwhite);
+  char **two_block = join(_spwhite, _spblack);
+  
+
+  interpreter(two_block);
 }
