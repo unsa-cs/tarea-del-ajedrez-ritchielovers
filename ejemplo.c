c0fea796d8d23652a30273bd0788ae2d7cccdfcf
Marcelo Quina <mquinad@unsa.edu.pe>
2024-10-04 23:24:38 -0500

    viendo que el ejercicio 2 es el inverso del ejercicio 1, se tuvo la idea de usar la funcion reverse en el resultado del ejercicio 1, dandole como parametro la funcion de repeteH , se logro dar solucion al ejercicio 2 de otra manera

--- ejemplo.c
+++ ejemplo.c
@@ -3,10 +3,10 @@
 
 void display() {
   
-  
   char **_spwhite = whiteSquare; //un casillero en blanco
   char **_spblack = reverse(_spwhite); //un casillero en negro
-  char **two_block = join(_spblack, _spwhite); //se cambio de lugar para que ahora empieze con el casillero negro
-  char **fila = repeatH(two_block,4); //se multiplica por 4 para que sean 8 casilleros
+  char **two_block = join(_spwhite, _spblack); //se cambio de lugar para que ahora empieze con el casillero negro
+  char **fila = reverse(repeatH(two_block,4)); //se multiplica por 4 para que sean 8 casilleros
+
   interpreter(fila);
 }
