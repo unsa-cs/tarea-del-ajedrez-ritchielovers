777813a3f45e8d951d83b7bf830ef3024434681d
Roberto Enao <renao@unsa.edu.pe>
2024-10-04 22:18:58 -0500

    Se probo la funcion rotateR junto a flipV

--- ejemplo.c
+++ ejemplo.c
@@ -6,6 +6,7 @@ void display() {
   char **_black_knight = reverse(_knight);
   char **two_knight = join(_knight, _black_knight);
   char **flipv_two_knight = flipV(two_knight);
+  /*
   char **four_knight = join(two_knight, flipv_two_knight);
   char **fliph_four_knight = flipH(four_knight);
   char **eight_knight = join(four_knight, fliph_four_knight);
@@ -13,5 +14,7 @@ void display() {
   char **thirtytwo_knight = up(sixteen_knight, reverse(sixteen_knight));
   char **sixtyfour_knight = up(thirtytwo_knight, thirtytwo_knight);
   char **rotatel_knight = rotateL(_knight);
-  interpreter(rotatel_knight);
+  */
+  char **flipv_rotater_two_knight = rotateR(flipv_two_knight);
+  interpreter(flipv_rotater_two_knight);
 }
