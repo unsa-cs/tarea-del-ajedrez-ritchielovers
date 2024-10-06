ab1fa07f81f476d1707a95a7f1452fbe40b6167b
JoseLuisCalizaya <joseluisataqu@gmail.com>
2024-10-05 20:54:32 -0500

    Avance del ejercicio 04 ,se comento el codigo del ejercicio02 porque aun no se hace uso de ello; se creo una variable de indentificador row_fila inicilizado con la figura torre, y luego se hace uso del join para poder concatenar las demas figuras del ajedrez, se logro tener la fila de piezas del ajedrez

--- ejercicio04.c
+++ ejercicio04.c
@@ -2,10 +2,20 @@
 #include "figures.h"
 
 void display() {
-  char **_spwhite = whiteSquare;       // un casillero en blanco
-  char **_spblack = reverse(_spwhite); // un casillero en negro
-  char **two_block = join(_spwhite, _spblack);
-  char **fila = reverse(repeatH(two_block, 4));
+  // char **_spwhite = whiteSquare;       // un casillero en blanco
+  // char **_spblack = reverse(_spwhite); // un casillero en negro
+  // char **two_block = join(_spwhite, _spblack);
+  // char **fila = reverse(repeatH(two_block, 4));
 
-  interpreter(fila);
+  char **row_figures = rook;
+
+  row_figures = join(row_figures, knight);
+  row_figures = join(row_figures, bishop);
+  row_figures = join(row_figures, queen);
+  row_figures = join(row_figures, king);
+  row_figures = join(row_figures, bishop);
+  row_figures = join(row_figures, knight);
+  row_figures = join(row_figures, rook);
+
+  interpreter(row_figures);
 }
