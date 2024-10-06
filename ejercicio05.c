956d000bf51ba59b120f7b94166839ce7645e295
Marcelo Quina <mquinad@unsa.edu.pe>
2024-10-05 21:13:52 -0500

    Avance ejercicio5 : se creo el archivo ejercicio05.c teniendo como base el ejercicio04.c ya que ambos ejericios se realizan de forma similar

---
+++ ejercicio05.c
@@ -0,0 +1,25 @@
+#include "chess.h"
+#include "figures.h"
+
+void display() {
+  char **_spwhite = whiteSquare;       // un casillero en blanco
+  char **_spblack = reverse(_spwhite); // un casillero en negro
+  char **two_block = join(_spwhite, _spblack);
+  char **fila = reverse(repeatH(two_block, 4));
+
+  char **row_figures = rook;
+   
+  //se concatenan las imagenes por separado en la variable row_figures
+  row_figures = join(row_figures, knight);
+  row_figures = join(row_figures, bishop);
+  row_figures = join(row_figures, queen);
+  row_figures = join(row_figures, king);
+  row_figures = join(row_figures, bishop);
+  row_figures = join(row_figures, knight);
+  row_figures = join(row_figures, rook);
+
+  //se superpone las figuras a los casilleros 
+  char**result = superImpose(row_figures, fila);
+
+  interpreter(result);
+}
