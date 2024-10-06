2b4cea46a4c38525911c2724094308e1f9a9196e
Marcelo Quina <mquinad@unsa.edu.pe>
2024-10-06 15:04:55 -0500

    Avance ejercicio 8 : Se intento crear el tablero de ajedrez usando 2 FOR anidados para hacer el recorrido de la matriz pero no salio como se esperaba

--- ejercicio08.c
+++ ejercicio08.c
@@ -10,4 +10,27 @@ void display(){
                             {1,0,0,0,0,0,0,0},
                             {0,0,0,0,0,0,1,0}, 
                             {0,0,0,0,1,0,0,0}};
+  char** square;
+  char** row;
+  char** chess_table;
+  for(int i = 0 ; i < 8 ; ++i){
+    if(i%2==0){
+      square=reverse(whiteSquare);
+    }else{
+      square=whiteSquare;
+    }
+    for(int j = 0 ;  j < 8 ; ++j){
+      if(j==0){
+        row=square;
+      } else{
+        row=join(row,square);
+      }
+    }
+    if(i==0){
+      chess_table=row;
+    }else{
+      chess_table=up(chess_table,row);
+    }
+  }
+  interpreter(chess_table);
 }
