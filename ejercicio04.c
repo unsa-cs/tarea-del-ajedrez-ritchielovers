8b3459b9e812828a40c9867cfb3b16053c433461
JoseLuisCalizaya <joseluisataqu@gmail.com>
2024-10-05 20:39:13 -0500

    Se crea un nuevo archivo ejercicio04.c con el codigo base del ejercicio02.c porque es un buen punto de inicio

---
+++ ejercicio04.c
@@ -0,0 +1,11 @@
+#include "chess.h"
+#include "figures.h"
+
+void display() {
+  char **_spwhite = whiteSquare;       // un casillero en blanco
+  char **_spblack = reverse(_spwhite); // un casillero en negro
+  char **two_block = join(_spwhite, _spblack);
+  char **fila = reverse(repeatH(two_block, 4));
+
+  interpreter(fila);
+}
