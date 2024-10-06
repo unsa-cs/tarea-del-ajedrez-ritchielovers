9fdfceead9d195826d63eb572670f642360107d4
CHOCEK <chanaricu@unsa.edu.pe>
2024-10-06 16:09:54 -0500

    Avance ejercicio 09: Se le asigno un identificador a cada pieza de ajedrez en base al indice del arreglo que creamos anteriormente, ademas de eso se logro dibujar un tablero de ajedrez con solo fichas blancas haciendo uso de la funcion superImpose, aun se sigue pensando como diferenciar las fichas negras

--- ejercicio09.c
+++ ejercicio09.c
@@ -1,46 +1,49 @@
 #include "chess.h"
 #include "figures.h"
 
-void display(){
-  int table[8][8] = {{0,0,0,0,0,0,0,0},
-                            {0,0,0,0,0,0,0,0},
-                            {0,0,0,0,0,0,0,0},
-                            {0,0,0,0,0,0,0,0},
-                            {0,0,0,0,0,0,0,0},
-                            {0,0,0,0,0,0,0,0},
-                            {0,0,0,0,0,0,0,0}, 
-                            {0,0,0,0,0,0,0,0}};
-  char** fichas[6]={pawn,knight,bishop,rook,queen,king};
-  char** square;
-  char** row;
-  char** chess_table;
-  char** ficha;
+void display() {
+  // 0 = tablero, vacio
+  // 1 = peon
+  // 2 = caballo
+  // 3 = alfil
+  // 4 = torre
+  // 5 = reina
+  // 6 = rey
+  int table[8][8] = {{4, 2, 3, 5, 6, 3, 2, 4}, {1, 1, 1, 1, 1, 1, 1, 1},
+                     {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0},
+                     {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0},
+                     {1, 1, 1, 1, 1, 1, 1, 1}, {4, 2, 3, 5, 6, 3, 2, 4}};
+  char **fichas[6] = {pawn, knight, bishop, rook, queen, king};
+  char **square;
+  char **row;
+  char **chess_table;
+  char **ficha;
 
-  for(int i = 0 ; i < 8 ; ++i){
-    if(i%2==0){
-      square=reverse(whiteSquare);
-    }else{
-      square=whiteSquare;
+  for (int i = 0; i < 8; ++i) {
+    if (i % 2 == 0) {
+      square = reverse(whiteSquare);
+    } else {
+      square = whiteSquare;
     }
-    for(int j = 0 ;  j < 8 ; ++j){
-        
-      if(j%2==0){
-        ficha=square;
-      }else{
-        ficha=reverse(square);
+    for (int j = 0; j < 8; ++j) {
+      if (j % 2 == 0) {
+        ficha = square;
+      } else {
+        ficha = reverse(square);
       }
 
-      if(j==0){
-        row=ficha;
-      }else{
-        row=join(row,ficha);
-      }
+      ficha = superImpose(fichas[table[i][j] - 1], ficha);
 
+      if (j == 0) {
+        row = ficha;
+      } else {
+        row = join(row, ficha);
+      }
     }
-    if(i==0){
-      chess_table=row;
-    }else{
-      chess_table=up(chess_table,row);
+    if (i == 0) {
+      chess_table = row;
+    } else {
+      chess_table = up(chess_table, row);
     }
   }
 
