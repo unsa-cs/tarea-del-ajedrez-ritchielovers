94a52b741717b50bb5568aa9fbe4a45676a41e06
SergioCalcinaMuchica <scalcinamu@unsa.edu.pe>
2024-10-05 21:51:46 -0500

    Avance ejercicio 6, se agrego la fila de peones blancos y la fila de las fichas blancas. Se unieron las filas mediante la funcion up

--- ejercicio06.c
+++ ejercicio06.c
@@ -5,7 +5,8 @@ void display() {
   char **_spwhite = whiteSquare;       // un casillero en blanco
   char **_spblack = reverse(_spwhite); // un casillero en negro
   char **two_block = join(_spwhite, _spblack);
-  char **fila = reverse(repeatH(two_block, 4));
+  char **fila_start_white = repeatH(two_block, 4);
+  char **fila_start_black = reverse(fila_start_white); 
 
   char **row_figures = rook;
    
@@ -18,8 +19,12 @@ void display() {
   row_figures = join(row_figures, knight);
   row_figures = join(row_figures, rook);
 
-  //se superpone las figuras a los casilleros
-  char**result = superImpose(row_figures, fila);
+  char **row_peones = repeatH(pawn,8);
+  row_peones = superImpose(row_peones,fila_start_white);
+
+  char **row_whites = superImpose(row_figures, fila_start_black);
+  
+  char **result = up(row_whites, row_peones);  
 
   interpreter(result);
 }
