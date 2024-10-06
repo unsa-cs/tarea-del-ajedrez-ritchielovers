8983c5aded746cdc759e5189bf49f0e495e7c834
Roberto Enao <renao@unsa.edu.pe>
2024-10-05 23:46:45 -0500

    Se pudo crear una fila con la figura de una caballo en al posicion estandar, luego se utilizo rotateL() para girar el caballo en sentido antihorario, para crear esta fila, primero se creo la mitad izquierda y despues la mitad derecha

---
+++ ejercicio07.c
@@ -0,0 +1,17 @@
+#include "chess.h"
+#include "figures.h"
+
+void display() {
+  char **_spwhite = whiteSquare;
+  char **knight_ = superImpose(knight, reverse(_spwhite));
+  char **_spblack = reverse(_spwhite);
+  char **two_block = join(_spwhite, _spblack);
+  char **fila = repeatH(two_block, 4);
+  char **two_fila = up(reverse(fila), fila);
+  char **knight_beside_spwhite = join(knight_, _spwhite);
+  char **left_mid = join(knight_beside_spwhite, reverse(two_block));
+  char **spblack_beside_knight = join(_spblack, rotateL(reverse(knight_)));
+  char **right_mid = join(reverse(two_block), spblack_beside_knight);
+  char **first_row_with_knight = join(left_mid, right_mid);
+  interpreter(first_row_with_knight);
+}
